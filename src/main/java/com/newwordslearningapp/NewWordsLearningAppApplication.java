36fd6155fa2919b1d5ae8e5ff97f71a5f9f07c1e
OlgaJak <jakimenko83@mail.ru>
2023-07-30 13:47:08 +0300

    first commit

---
+++ src/main/java/com/newwordslearningapp/NewWordsLearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.newwordslearningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWordsLearningAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewWordsLearningAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newwordslearningapp/NewWordsLearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newwordslearningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewWordsLearningAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
