422e9ec8c362bdf3947c98aade1d6402b6e0c773
OlgaJak <jakimenko83@mail.ru>
2023-08-05 10:51:01 +0300

    created services for UserWordsRepository and UserProgressRepository

--- src/main/java/com/newwordslearningapp/service/UserProgressService.java
+++ src/main/java/com/newwordslearningapp/service/UserProgressService.java
@@ -1,4 +1,14 @@
 package com.newwordslearningapp.service;
 
+import com.newwordslearningapp.repository.UserLearnedWordsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserProgressService {
+    private UserLearnedWordsRepository userLearnedWordsRepository;
+@Autowired
+    public UserProgressService(UserLearnedWordsRepository userLearnedWordsRepository) {
+        this.userLearnedWordsRepository = userLearnedWordsRepository;
+    }
 }
