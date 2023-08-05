8273993c8471af0c2a98a20d005c1462d5092d6d
OlgaJak <jakimenko83@mail.ru>
2023-08-05 11:43:03 +0300

    created connection to API RandomWord

--- src/main/java/com/newwordslearningapp/NewWordsLearningAppApplication.java
+++ src/main/java/com/newwordslearningapp/NewWordsLearningAppApplication.java
@@ -3,11 +3,33 @@ package com.newwordslearningapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static com.newwordslearningapp.APIconnection.WordAPIConnector.getWordFromApi;
+
 @SpringBootApplication
 public class NewWordsLearningAppApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(NewWordsLearningAppApplication.class, args);
+
+
+        //testing if 1 api giving random words is working
+        try {
+            String randomWord = getWordFromApi();
+            // You can now use the 'randomWord' variable to do something with the word from the API.
+            // For example, print it:
+            System.out.println("Random word from API: " + randomWord);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
     }
 
 }
