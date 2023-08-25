afb5541e304134dc663501a4d9ced8ea5df7e064
admin <boot309@gmail.com>
2023-08-25 21:22:27 +0300

    added some styling to quiz

--- src/main/java/com/newwordslearningapp/APIconnection/WordAPIConnector.java
+++ src/main/java/com/newwordslearningapp/APIconnection/WordAPIConnector.java
@@ -14,7 +14,7 @@ public class WordAPIConnector {
     public static String getWordFromApi() throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://random-words5.p.rapidapi.com/getRandom"))
-                .header("X-RapidAPI-Key", "87fab75d84msh24e19b95986673fp18b478jsnf7fe7ac6f2d7")
+                .header("X-RapidAPI-Key", "dcf6e73675msh173f440d0fc059bp132ba0jsn07fda1c5e255")
                 .header("X-RapidAPI-Host", "random-words5.p.rapidapi.com")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
