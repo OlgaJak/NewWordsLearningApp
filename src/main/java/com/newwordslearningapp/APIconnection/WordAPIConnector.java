a313315e5ae40ffce1e5c53f1ba82f5cbc811d27
admin <boot309@gmail.com>
2023-08-27 15:41:06 +0300

    quiz page css done, except for radio buttons

--- src/main/java/com/newwordslearningapp/APIconnection/WordAPIConnector.java
+++ src/main/java/com/newwordslearningapp/APIconnection/WordAPIConnector.java
@@ -14,7 +14,7 @@ public class WordAPIConnector {
     public static String getWordFromApi() throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://random-words5.p.rapidapi.com/getRandom"))
-                .header("X-RapidAPI-Key", "74cc2fde4bmsh5c9fc40065a46d8p1e0b6cjsn18fd8a19d68f")
+                .header("X-RapidAPI-Key", "89cf1ee5bdmshc4fa1bfbd1835b5p108525jsnf23980e4830e")
                 .header("X-RapidAPI-Host", "random-words5.p.rapidapi.com")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
